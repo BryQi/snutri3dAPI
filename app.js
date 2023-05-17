006b93f277ab5507797c3eaa894b9f5bc404d47e
brayanMASTER <brayan.correa@unl.edu.ec>
2023-05-16 23:06:40 -0500

    Login

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ require('./routes/authentication')(app);
 // Conexión con la base de datos
 mongoose.set('strictQuery', true);
 //const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSS}@clustersnutri3dbd.9skyq9n.mongodb.net/${process.env.BD}?retryWrites=true&w=majority`;
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSS}@snutri3dcluster.brblj4q.mongodb.net/${process.env.BD}?retryWrites=true&w=majority`;
+//const uri = "mongodb://localhost:27017";
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
